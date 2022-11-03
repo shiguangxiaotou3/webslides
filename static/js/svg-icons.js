b8e9b549b9bd7a06df68f68934cbd1ee6f930e32
shiguangxiaotou3 <wanlong757402@outlook.com>
2022-11-03 19:54:43 +0800

    2022-11-3

--- static/js/svg-icons.js
+++ static/js/svg-icons.js
@@ -1,12 +1,92 @@
-/*
- * Thanks fontastic.me (Font Awesome 4.4 as SVG Icons).
- * Do you want to use other icons?
- * Go to https://fontastic.me > Create account > Select icons > Customize > Publish (SVG Sprite) > Paste .js here
- */
 
-/*
-Important!
-/css/svg-icons.css is required. 
-*/
+(function (DOM, elements, c, svgData) {
 
-(function(a,b,c,d){function e(b,c){if(c){var d=c.getAttribute('viewBox'),e=a.createDocumentFragment(),f=c.cloneNode(true);if(d)b.setAttribute('viewBox',d);while(f.childNodes.length)e.appendChild(f.childNodes[0]);b.appendChild(e);}}function f(){var b=this,c=a.createElement('x'),d=b.s;c.innerHTML=b.responseText;b.onload=function(){d.splice(0).map(function(a){e(a[0],c.querySelector('#'+a[1].replace(/(\W)/g,'\\$1')));});};b.onload();}function g(){var a;while((a=b[0])){var e=a.parentNode,h=a.getAttribute('xlink:href').split('#')[1],i='https://file.myfontastic.com/bLfXNBF36ByeujCbT5PohZ/sprites/1477146123.svg';e.removeChild(a);var j=d[i]=d[i]||new XMLHttpRequest();if(!j.s){j.s=[];j.open('GET',i);j.onload=f;j.send();}j.s.push([e,h]);if(j.readyState===4)j.onload();}c(g);}g();})(document,document.getElementsByTagName('use'),window.requestAnimationFrame||window.setTimeout,{});
+    /**
+    * 创建svg元素
+    */
+    function createSvgTag(element, c) {
+        if (c) {
+            var d = c.getAttribute('viewBox'),
+                e = DOM.createDocumentFragment(),
+                f = c.cloneNode(true);
+            if (d){
+                element.setAttribute('viewBox', d);
+            }
+            while (f.childNodes.length){
+                e.appendChild(f.childNodes[0]);
+            }
+
+            element.appendChild(e);
+        }
+    }
+
+    /**
+     加载svg 数据
+    */
+    function onloadSvgData() {
+        var elements = this;
+        var c = DOM.createElement('x');
+        var  svgData = elements.s;
+        c.innerHTML = elements.responseText;
+        elements.onload = function () {
+            svgData.splice(0).map(function (a) {
+                createSvgTag(
+                    a[0],
+                    c.querySelector('#' + a[1].replace(/(\W)/g, '\\$1'))
+                );
+            });
+        };
+        elements.onload();
+    }
+
+    /**
+     * 获取当前文件的url路由
+     */
+    function getUrl() {
+
+        var url = window.location.href;
+        if(url.indexOf("file:///") !== -1 ){
+            return 'https://file.myfontastic.com/bLfXNBF36ByeujCbT5PohZ/sprites/1477146123.svg';
+        }else{
+            var js = document.scripts;
+            var urlPath;
+            for (var x = 0; x < js.length; x++) {
+                if (js[x].src.indexOf("svg-icons.js") > -1) {
+                    urlPath = js[x].src.substring(0, js[x].src.lastIndexOf("/") + 1);
+                }
+            }
+            return urlPath+"1477146123.svg";
+        }
+    }
+
+    /**
+     * 请求svg数据，遍历DOM
+    */
+    function createSvg() {
+        var document;
+        while ((document = elements[0])) {
+            var svgTag = document.parentNode,
+                h = document.getAttribute('xlink:href').split('#')[1],
+                url =  getUrl();
+            svgTag.removeChild(document);
+            var http = svgData[url] = svgData[url] || new XMLHttpRequest();
+            if (!http.s) {
+                http.s = [];
+                http.open('GET',  url);
+                http.onload = onloadSvgData;
+                http.send();
+            }
+            http.s.push([svgTag, h]);
+            if (http.readyState === 4){
+             http.onload();
+            }
+        }
+        c(createSvg);
+    }
+    createSvg();
+})(
+    document,
+    document.getElementsByTagName('use'),
+    window.requestAnimationFrame || window.setTimeout,
+    {}
+);
